8f935ef21add3e3572627ed3900147fdd4386578
Imtiaz Ali <171873475+Imtiaz11223344@users.noreply.github.com>
2026-08-02 14:49:35 +0500

    Learn about the single and multi line comments

--- Day_01/hello.py
+++ Day_01/hello.py
@@ -1,1 +1,5 @@
 print("Hello To Day i am starting the python for revision and practices")
+#print Number and this is single line comment
+print(10)
+'''This is example of multi line comment'''
+print('Hello World')
