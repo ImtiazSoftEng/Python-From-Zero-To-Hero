6ff36e7f15b580ed77882e4f5677c9244371c237
Imtiaz Ali <171873475+ImtiazSoftEng@users.noreply.github.com>
2026-08-18 12:04:13 +0500

    Python Random Module

--- Day_04/data_types.py
+++ Day_04/data_types.py
@@ -4,3 +4,21 @@ num2 = 5.0
 print(num2, 'is of type', type(num2))
 num3 = 1 + 2j
 print(num3, 'is complex number?', isinstance(1 + 2j, complex))
+
+import random
+
+print(random.randrange(10, 20))
+
+list1 = ['a', 'b', 'c', 'd', 'e']
+
+# get random item from list1
+print(random.choice(list1))
+
+# Shuffle list1
+random.shuffle(list1)
+
+# Print the shuffled list1
+print(list1)
+
+# Print random element
+print(random.random())
