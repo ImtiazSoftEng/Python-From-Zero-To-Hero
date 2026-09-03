029b7c18f1bffdb0fcdf3eed5e6ff91f4f7a7b91
Imtiaz Ali <171873475+Imtiaz11223344@users.noreply.github.com>
2026-09-03 23:20:32 +0500

    Multiple Inheritance topic cleared and learned

---
+++ Day_08/multiple_inheritance.py
@@ -0,0 +1,59 @@
+class Mammal:
+    def mammal_info(self):
+        print("Mammals can give direct birth.")
+
+class WingedAnimal:
+    def winged_animal_info(self):
+        print("Winged animals can flap.")
+
+class Bat(Mammal, WingedAnimal):
+    pass
+
+# create an object of Bat class
+b1 = Bat()
+
+b1.mammal_info()
+b1.winged_animal_info()
+
+#Example: Python Multilevel Inheritance
+class SuperClass:
+
+    def super_method(self):
+        print("Super Class method called")
+
+# define class that derive from SuperClass
+class DerivedClass1(SuperClass):
+    def derived1_method(self):
+        print("Derived class 1 method called")
+
+# define class that derive from DerivedClass1
+class DerivedClass2(DerivedClass1):
+
+    def derived2_method(self):
+        print("Derived class 2 method called")
+
+# create an object of DerivedClass2
+d2 = DerivedClass2()
+
+d2.super_method()  # Output: "Super Class method called"
+
+d2.derived1_method()  # Output: "Derived class 1 method called"
+
+d2.derived2_method()  # Output: "Derived class 2 method called"
+
+#Method Resolution Order
+class SuperClass1:
+ def info(self):
+        print("Super Class 1 method called")
+
+class SuperClass2:
+    def info(self):
+        print("Super Class 2 method called")
+
+class Derived(SuperClass1, SuperClass2):
+    pass
+
+d1 = Derived()
+d1.info()  
+
+# Output: "Super Class 1 method called"
