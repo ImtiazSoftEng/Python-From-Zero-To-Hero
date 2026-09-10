7548070b488456bcbace9cb733623065f660c5ba
Imtiaz Ali <171873475+ImtiazSoftEng@users.noreply.github.com>
2026-09-10 12:50:23 +0500

    Basic learn about the time and date in python

---
+++ Day_10/current_datetime.py
@@ -0,0 +1,11 @@
+#Example 1: Get Current Date and Time
+import datetime
+# get the current date and time
+now = datetime.datetime.now()
+print(now)
+
+#Example 2: Get Current Date
+
+#get current date
+current_date = datetime.date.today()
+print(current_date)
