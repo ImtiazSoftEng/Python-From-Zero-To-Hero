4f08291f6c41bd4a1f3bac01552b926cb65bd870
Imtiaz Ali <171873475+ImtiazSoftEng@users.noreply.github.com>
2026-09-11 10:22:14 +0500

    learn about tje strptime

---
+++ Day_10/strptime.py
@@ -0,0 +1,24 @@
+#Example 1: string to datetime object
+from datetime import datetime
+
+date_string = "21 June, 2018"
+
+print("date_string =", date_string)
+print("type of date_string =", type(date_string))
+
+date_object = datetime.strptime(date_string, "%d %B, %Y")
+
+print("date_object =", date_object)
+print("type of date_object =", type(date_object))
+#Example 2: string to datetime object
+from datetime import datetime
+
+dt_string = "12/11/2018 09:15:32"
+
+# Considering date is in dd/mm/yyyy format
+dt_object1 = datetime.strptime(dt_string, "%d/%m/%Y %H:%M:%S")
+print("dt_object1 =", dt_object1)
+
+# Considering date is in mm/dd/yyyy format
+dt_object2 = datetime.strptime(dt_string, "%m/%d/%Y %H:%M:%S")
+print("dt_object2 =", dt_object2)
